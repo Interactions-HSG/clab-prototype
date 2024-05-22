5576a015bc32241ded99d07595103e4fea36186a
Alessandro Giugno <alessandro.giugno@gmx.de>
2024-05-22 14:14:40 +0200

    chore(clab_prototype): config facilitates reuseability

---
+++ clab_prototype/config.py
@@ -0,0 +1,4 @@
+import os
+
+PROTO_HOSTNAME=os.getenv("PROTO_HOSTNAME", "localhost")
+PROTO_PORT=os.getenv("PROTO_PORT", "8050")
--- clab_prototype/main.py
+++ clab_prototype/main.py
@@ -1,26 +1,33 @@
+#!/usr/bin/env python
 from pathlib import Path
 
 import dash
 from dash import (
     dcc,
-    html
+    html,
+    DiskcacheManager
 )
 from dash.dependencies import (
     Input,
     Output,
 )
-
+import diskcache
 from clab_ceis import shop, ceis
+from recipe_synth.app import app as rs_app
+
+import config as cfg
 
 class CeisDemoHome():
     _shop = None
     _monitor = None
+    _planner = None
     _app = None
 
     def __init__(self, app: dash.Dash) -> None:
         self._app = app
         self._shop = shop.CeisShop(app)
         self._monitor = ceis.CeisMonitor(app)
+        self._planner = rs_app.RecipeSynthesizer(app)
 
     # Function to read Markdown content from a file
     def read_markdown_file(self, filename):
@@ -36,12 +43,15 @@ class CeisDemoHome():
             dcc.Markdown(children=self.read_markdown_file("README.md"))
         ])
 
+        # TODO: Makes this a dash page instead
+        # https://dash.plotly.com/urls
         # Define the app layout, including tabs for switching between views
         self._app.layout = html.Div([
             dcc.Tabs(id='tabs', value='home', children=[
                 dcc.Tab(label='Home', value='home'),
                 dcc.Tab(label='Shop', value='shop'),
-                dcc.Tab(label='CEM', value='monitor')
+                dcc.Tab(label='CEM', value='monitor'),
+                dcc.Tab(label='Planner', value='planner')
             ]),
             html.Div(id='tabs-content')
         ])
@@ -56,16 +66,22 @@ class CeisDemoHome():
                 return self._shop.layout
             elif tab == 'monitor':
                 return self._monitor.layout
+            elif tab == 'planner':
+                return self._planner.layout
 
 if __name__ == '__main__':
     # Initialize Dash app
-    app = dash.Dash(__name__, suppress_callback_exceptions=True)
+    # TODO: Move this to OS specific caches
+    cache = diskcache.Cache(Path(__file__).parent / "cache")
+    background_callback_manager = DiskcacheManager(cache)
+    app = dash.Dash(
+        __name__,
+        suppress_callback_exceptions=True,
+        background_callback_manager=background_callback_manager
+    )
     demo_home = CeisDemoHome(app)
     demo_home.make_app()
     
-    # ceis_callbacks.get_callbacks(app)
-    # ceis.get_route(app.server)
-
-    # shop_callbacks.get_callbacks(app)
-    
-    app.run_server(host="localhost", port="8051", debug=True)
+   
+    # app.run_server(host="localhost", port="8051", debug=True)
+    app.run_server(host=cfg.PROTO_HOSTNAME, port=cfg.PROTO_PORT, debug=True)
